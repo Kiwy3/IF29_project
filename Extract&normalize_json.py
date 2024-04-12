9216e6ce1771e2988d630473fa8590903657fae8
Kiwy3 <nathan.davouse@gmail.com>
2024-04-12 14:21:45 +0200

    Add doc & start EDA

--- Extract&normalize_json.py
+++ Extract&normalize_json.py
@@ -18,8 +18,24 @@ data = pd.json_normalize(data)
 
 """Count NaN Value by columns"""
 col = data.columns
+Nacount = []
 for i in col:
-    c = data[i].isna().sum()
+    Nacount.append(data[i].isna().sum())
+    
+Stud = pd.DataFrame(col,columns=["Attribut"])
+"NA count "
+NA_lim = 1600
+Stud["NA_count"] = Nacount
+Stud["NA_filter"] = Stud.NA_count.apply(lambda x :1  if x >NA_lim  else 0)
+"""User attribute"""
+Stud["user_filter"] = Stud.Attribut.apply(lambda x :1  if x.find("user.")==0  else 0)
+user_attributes = Stud.query("user_filter==1").drop("NA_filter",axis=1)
+"""Count point"""
+Stud["lvl"] = Stud.Attribut.apply(lambda x :x.count("."))
+
+Stud["origine"] = Stud.Attribut.apply(lambda x : x.split(".")[0]  if x.count(".")>0  else "/")
+
+
     
     
 
