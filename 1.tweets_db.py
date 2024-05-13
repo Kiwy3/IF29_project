dc6a425a2b0751bb1a13cbb84c7e354c82e5417b
Nathan Davouse <nathan.davouse@gmail.com>
2024-05-13 10:45:15 +0200

    Update tweet_db and user_db
    Upgrade the aggregation

--- 1.tweets_db.py
+++ 1.tweets_db.py
@@ -1,3 +1,12 @@
+"""
+
+Import many json file of tweets to only one mongo database
+In the case  study of IF29 class
+author : Nathan Davouse
+
+"""
+
+#Import librairies
 import pymongo
 from pymongo import MongoClient
 import jsonlines
@@ -6,17 +15,22 @@ import os
 #connect from mongoDB
 client = MongoClient("localhost", 27017)
 db = client["IF29"]
-collec = db.wc_import
+collec = db.tweets_global
 
+#Initialisation
 folder_path = "C:\\Users\\Nathan\\IF29_project\\raw"
 total = len(os.listdir(folder_path))
 i=0
+#loop on everyfile of the folder
 for filename in os.listdir(folder_path):
     if os.path.isfile(os.path.join(folder_path, filename)):
+        #open current file with jsonlines
         with jsonlines.open(os.path.join(folder_path, filename)) as f:
             for line in f:
-                collec.insert_one(line)
+                collec.insert_one(line) #add one document on the collection
+            #follow the number of file and print the current step  
             i=i+1
             print("file "+ str(filename)+" imported // ",i ,"/",total)
-              
-client.close()
+
+#Close the connection with mongodb             
+client.close()   
---
+++ 2.user_db.py
@@ -0,0 +1,85 @@
+"""
+
+Make user database from tweet database with mongo aggregate
+In the case  study of IF29 class
+author : Nathan Davouse
+
+"""
+#Import librairies
+from pymongo import MongoClient
+import time
+
+#Connection with mongoDB
+client = MongoClient("localhost", 27017)
+db = client["IF29"]
+collec = db.tweets_global
+
+#find the most recent date
+"""
+first_pip = [
+    {"$project": {"user.created_at": 1}},
+    {"$sort":{"user.created_at":1}},
+    {"$limit":1}
+
+]
+
+max_date_cursor = collec.aggregate(first_pip)
+for date in max_date_cursor:
+    max_date = date["user"]["created_at"]"""
+
+#Make the aggregation pipeline
+pipeline = [
+    {"$sort":{"current_time":1}}, #to allow $last to effectively be the last
+    {"$limit": 3}, #if we need to test it on few lines
+     
+    #Add nb of # and @ on each tweet
+    {"$addFields":{"hash_count" : {"$subtract": [{"$size" : {"$split" : ["$text","#"]} },1 ]  }},  },
+    {"$addFields":{"arobase_count" : {"$subtract": [{"$size" : {"$split" : ["$text","@"]} },1 ]  }},  },
+    #manage text field
+    {"$addFields":{"tweet_url_bool" : { "$cond" : [{"$eq" : ["$user.url","null"]},True,False]}},  },
+    {"$addFields":{"tweet_len_description" : { "$cond" : [{"$eq" : ["$user.description","null"]},0,{"$strLenBytes" : "$user.description"}]}},  },
+    #group by user 
+    {"$group":{ #user global information
+               "_id":"$user.id",
+               "name" : {"$last":"$user.name"},
+               "created_date" : {"$last":"$user.created_at"},
+               #"datediff" : {"$avg":{"$dateDiff": {"startDate" : "$created_date","endDate" : {"$max" : "$created_date" },"unit" : "day" }}},
+               "verified":{"$last":"$user.verified"},
+               #user numeric information
+               "friend_nb":{"$avg":"$user.friends_count"},
+               "listed_nb":{"$avg":"$user.listed_count"},
+               "follower_nb":{"$avg":"$user.followers_count"},
+               "favorites_nb":{"$avg":"$user.favourites_count"},
+               #Text Fields
+               "url_bool" : {"$max" :"$tweet_url_bool" },
+               "len_description" : {"$avg" : "$tweet_len_description"},
+               #tweet stats
+               "tweet_nb" : {"$sum":1},
+               "hash_nb" : {"$sum" : "$hash_count"},
+               "hash_avg" : {"$avg" : "$hash_count"},
+               "retweet_avg" : {"$avg" : "$retweet.count"},
+               "tweet_user_count":{"$max":"$user.statuses_count"}
+               
+               }}
+
+    #Export it on another database
+     ,{"$out" : "user_db_V1"}
+]
+
+st = time.localtime() #to collect the time of start
+collec.aggregate(pipeline)
+end = time.localtime() #to collect the time of end of the aggregation
+
+#print the doc for test
+test = collec.aggregate(pipeline)
+for doc in test:
+    print(doc)
+
+#close the mongodb connection
+client.close()
+
+#print the time used to make the aggregation
+sec = end.tm_sec - st.tm_sec + 60*(end.tm_min - st.tm_min)
+sec_res = sec%60
+minu = (sec-sec_res)/60
+print("temps de run : ",minu,"minutes et ",sec_res," secondes")
