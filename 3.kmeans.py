61cc895e2adb2179a22d5343963997e085b0ab9c
Nathan Davouse <nathan.davouse@gmail.com>
2024-05-22 11:53:51 +0200

    add ACP and plot
    + to do list in txt file

--- 3.kmeans.py
+++ 3.kmeans.py
@@ -24,39 +24,43 @@ from pymongo import MongoClient
 import pandas as pd
 client = MongoClient("localhost", 27017)
 db = client["IF29"]
-#collec = db.user_db_V1 #whole database
-collec = db.user_db_sample #small db with 100 tweets
+collec = db.user_db_V1 #whole database
+#collec = db.user_db_sample #small db with 100 tweets
 data = pd.DataFrame(list(collec.find()))
 time_2 = time.localtime()
 
 #choose feature
 features = list(data.columns)
 del features[0:3] #to remove id, name and created_date
-features = ["hash_avg", "friend_nb", "listed_nb", "follower_nb", "favorites_nb", "tweet_nb", "tweet_nb"]
+features = ["verified", "friend_nb", "listed_nb", "follower_nb", "favorites_nb","url_bool","len_description","tweet_nb","hash_avg","at_avg","tweet_user_count"]
 X = data[features]
+debug = ["url_bool","retweet_avg"]
 
 
 #normalize data
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_sc = scaler.fit_transform(X)
-T = pd.DataFrame(X_sc)
+
+#PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2) 
+pca_data = pca.fit_transform(X_sc)
+data["PCA_1"] = pca_data[:,0]
+data["PCA_2"] = pca_data[:,1]
 
 #Perform K-means
 from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters = 2, init = 'k-means++')
+kmeans = KMeans(n_clusters = 2, init = 'k-means++',n_init = 100)
 kmeans.fit(X_sc)
+
+#plot
+import matplotlib.pyplot as plt
+plt.scatter(data[data.labels == 0].PCA_1,data[data.labels == 0].PCA_2,c="red")
+plt.scatter(data[data.labels == 1].PCA_1,data[data.labels == 1].PCA_2,c="blue")
+
+#Export
 data["labels"] = kmeans.labels_
-
-for i in range(len(X)):
-    collec.update_one(
-    {"_id" : data.loc[i,"_id"].value() },
-    {"$set" : {"kmeans_label" : data.loc[i,"labels"].value()}}
-)
-
-#Add label to mongodb
-"""
-docs = collec.update_many(
-    {"_id" : data["_id"] },
-    {"$set" : {"kmeans_label" : data["labels"]}}
-)"""
+export = data.loc[:,["labels","_id"]]
+#db.KM.drop()
+#db.KM.insert_many(export.to_dict('records'))
