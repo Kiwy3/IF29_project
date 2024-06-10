194bff43a390a80d7390973522636e9bc618d9e1
Nathan Davouse <nathan.davouse@gmail.com>
2024-06-10 14:33:02 +0200

    correct specific symbol count & ff_ratio
    + exchange aggressivity and visibility to be in line with

--- 2.user_db.py
+++ 2.user_db.py
@@ -16,64 +16,65 @@ db = client["IF29"]
 collec = db.tweets_global
 
 #define the comparaison date
-max_date = datetime(2018, 7, 1)
+max_date = datetime(2018, 7, 18) #one day after last tweet
 
 #Make the aggregation pipeline
 pipeline = [
-    {"$sort":{"current_time":1}}, #to allow $last to effectively be the last
-    #{"$limit": 100}, #if we need to test it on few lines
-     
-    #Add nb of # and @ on each tweet
-    {"$addFields":{"hash_count" : {"$subtract": [{"$size" : {"$split" : ["$text","#"]} },1 ]  }},  },
-    {"$addFields":{"at_count" : {"$subtract": [{"$size" : {"$split" : ["$text","@"]} },1 ]  }},  },
-    #manage text field
-    {"$addFields":{"tweet_url_bool" : { "$cond" : [{"$eq" : ["$user.url","null"]},True,False]}},  },
-    #{"$addFields":{"tweet_len_description" : { "$cond" : [{"$eq" : ["$user.description","null"]},{"$strLenBytes" : "$user.description"},0 ]}},  },
-    #mange user.created_at date fields
+    #{"$sort":{"current_time":1}}, #to allow $last to effectively be the last
+    {"$limit": 50}, #if we need to test it on few lines
+    {"$addFields":{"tweet_len_description" : { "$strLenCP": {"$ifNull" : ["$user.description",""] }}}},
+
+    #Count specific caracter inside the text of a tweet, using entities attribute
+    {"$addFields":{"tweet_hashtag_count" : {"$cond": { "if" : {"$isArray" : "$entities.hashtags"},"then" : {"$size":"$entities.hashtags"},"else" : 0}}},  },
+    {"$addFields":{"tweet_mention_count" : {"$cond": { "if" : {"$isArray" : "$entities.user_mentions"},"then" : {"$size":"$entities.user_mentions"},"else" : 0}}},  },
+    {"$addFields":{"tweet_urls_count" : {"$cond": { "if" : {"$isArray" : "$entities.urls"},"then" : {"$size":"$entities.urls"},"else" : 0}}},  },
+    {"$addFields":{"tweet_symbols_count" : {"$cond": { "if" : {"$isArray" : "$entities.symbols"},"then" : {"$size":"$entities.symbols"},"else" : 0}}},  },
+    #define the user date as a Date
     {"$addFields":{"user_date" : {"$dateFromString":{"dateString" : "$user.created_at"}}  }},
     #group by user 
     {"$group":{ #user global information
                "_id":"$user.id",
                "name" : {"$last":"$user.name"},
                "created_date" : {"$last":"$user_date"},
-               "verified":{"$last":"$user.verified"},
+               #user boolean
+               "verified":{"$max":"$user.verified"},
+               "protected":{"$max":"$user.protected"},
                #user numeric information
-               "friend_nb":{"$avg":"$user.friends_count"},
+               "friend_nb":{"$avg":"$user.friends_count"}, #Follow someone
                "listed_nb":{"$avg":"$user.listed_count"},
-               "follower_nb":{"$avg":"$user.followers_count"},
+               "follower_nb":{"$avg":"$user.followers_count"}, #is followed
                "favorites_nb":{"$avg":"$user.favourites_count"},
-               #Text Fields
-               "url_bool" : {"$max" :"$tweet_url_bool" },
-               "description" : {"$last" : "$user.description"},
+               "len_description" : {"$last" : "$tweet_len_description"},
+                #Specific caracters count
+               "hash_avg" : {"$avg" : "$tweet_hashtag_count"},
+               "mention_avg" : {"$avg" : "$tweet_mention_count"},
+               "url_avg" : {"$avg" : "$tweet_urls_count"},
+               "symbols_avg" : {"$avg" : "$tweet_symbols_count"},
                #tweet stats
                "tweet_nb" : {"$sum":1},
-               "hash_avg" : {"$avg" : "$hash_count"},
-               "at_avg" : {"$avg" : "$at_count"},
-               "retweet_avg" : {"$avg" : "$retweet.count"},
                "tweet_user_count":{"$max":"$user.statuses_count"} 
                }},
-    #Make the visibility attribute
+    #length of the user description
+    #User lifetime used to make new attribute
     {"$addFields": { "user_lifetime":{"$dateDiff": {"startDate" : "$created_date",
                                    "endDate" : max_date,
                                    "unit" : "day"}}     }},
+    # Add aggressivity
     {"$addFields" : {"tweet_frequency" : {"$divide" : ["$tweet_user_count","$user_lifetime"] }}},
     {"$addFields" : {"friend_frequency" : {"$divide" : ["$friend_nb","$user_lifetime"] }}},
-    {"$addFields" : {"visibility" : {"$add" : ["$tweet_frequency","$friend_frequency"] }}},
-    #Make the aggressivity attribute
-    {"$addFields" : {"Aggressivity" : {"$add" : ["$at_avg","$hash_avg"] }}},
+    {"$addFields" : {"aggressivity" : {"$add" : ["$tweet_frequency","$friend_frequency"] }}},
+    #Make the visibility attribute
+    {"$addFields" : {"visibility" : {"$add" : ["$mention_avg","$hash_avg","$url_avg","$symbols_avg"] }}},
+    #create a ff_ratio attribute
+    {"$addFields" : {"ff_ratio" : {"$divide" : ["$friend_nb","$follower_nb"] }}},
     #Export it on another database
-     #,{"$out" : "user_db_sample"} #Sample database for small test
-    {"$out" : "user_db_V2"}
+    {"$out" : "user_db_sample"} #Sample database for small test
+    #{"$out" : "user_db_V2"}
 ]
 
 st = time.localtime() #to collect the time of start
 collec.aggregate(pipeline)
 end = time.localtime() #to collect the time of end of the aggregation
-
-#print the doc for test
-"""test = collec.aggregate(pipeline)
-for doc in test:
-    print(doc)"""
 
 #close the mongodb connection
 client.close()
