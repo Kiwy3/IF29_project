f9c6d926a5c5ac32d81c6f4298b9a70c6965c830
Nathan Davouse <nathan.davouse@gmail.com>
2024-06-19 10:10:49 +0200

    sample 10% of the db

--- 4_1.kmeans.py
+++ 4_1.kmeans.py
@@ -5,57 +5,35 @@ In the case  study of IF29 class
 author : Nathan Davouse
 
 """
+from sklearn.cluster import KMeans
+from sklearn import metrics
+import matplotlib.pyplot as plt
 
 #connect from Mongo DB and import it on pandas
 from pymongo import MongoClient
 import pandas as pd
 client = MongoClient("localhost", 27017)
 db = client["IF29"]
-collec = db.user_db_V1 #whole database
-#collec = db.user_db_sample #small db with 100 tweets
+collec = db.user_db_pca #whole database
 data = pd.DataFrame(list(collec.find()))
-max_date = data.created_date.max()
-data["diff"] = data.created_date.apply(lambda x : (x - max_date).seconds)
+id_list = data.pop("_id")
+X = data
+# Calculer les scores de silhouette pour k variant de 2 à 10
+silhouette_scores = []
+for k in range(2, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42,verbose=1)
+    kmeans.fit(X)
+    labels = kmeans.labels_
+    silhouette_scores.append(metrics.silhouette_score(X, labels))
+    print("S_score : ",k,"/9")
+# Afficher les scores de silhouette
+print("Silhouette Scores for each k:", silhouette_scores)
 
-
-#choose feature
-features = ["verified", "friend_nb", "listed_nb", "follower_nb", "favorites_nb","url_bool","len_description","tweet_nb","hash_avg","at_avg","tweet_user_count","diff"]
-X = data[features]
-debug = ["url_bool","retweet_avg"] #attribute to debug on file 2.user_db.py
-
-#normalize data
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-X_sc = scaler.fit_transform(X)
-
-#PCA
-from sklearn.decomposition import PCA
-pca = PCA(n_components=3) 
-pca_data = pca.fit_transform(X_sc)
-data["PCA_1"] = pca_data[:,0]
-data["PCA_2"] = pca_data[:,1]
-data["PCA_3"] = pca_data[:,2]
-
-#Perform K-means
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters = 2, init = 'k-means++',n_init = 100)
-kmeans.fit(X_sc)
-data["labels"] = kmeans.labels_
-
-
-#plot
-import matplotlib.pyplot as plt
-plt.scatter(data[data.labels == 0].PCA_1,data[data.labels == 0].PCA_2,c="red",s=0.2)
-plt.scatter(data[data.labels == 1].PCA_1,data[data.labels == 1].PCA_2,c="blue",s=0.2)
+# Tracer les scores de silhouette
+plt.figure(figsize=(10, 6))
+plt.plot(range(2, 11), silhouette_scores, marker='o')
+plt.title("Silhouette Scores for KMeans Clustering")
+plt.xlabel("Number of Clusters")
+plt.ylabel("Silhouette Score")
+plt.grid(True)
 plt.show()
-
-#3D plot
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(data[data.labels == 0].PCA_1, data[data.labels == 0].PCA_2, data[data.labels == 0].PCA_3)
-ax.scatter(data[data.labels == 1].PCA_1, data[data.labels == 1].PCA_2, data[data.labels == 1].PCA_3)
-plt.show()
-
-#Export
-db.user_kmeans_pca.drop()
-db.user_kmeans_pca.insert_many(data.to_dict('records'))
