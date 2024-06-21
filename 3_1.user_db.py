2e0a6039c439a71ff7e0c21c69fce426ffc7a41a
Nathan Davouse <nathan.davouse@gmail.com>
2024-06-21 12:07:51 +0200

    update visibility definition
    add hashtag, url and mention costs

--- 3_1.user_db.py
+++ 3_1.user_db.py
@@ -18,10 +18,17 @@ collec = db.tweets_global
 #define the comparaison date
 max_date = datetime(2018, 7, 18) #one day after last tweet
 
+#caracter costs
+hashtag_cost = 7.84
+url_cost = 22.9
+mention_cost = 10.57
+
+
+
 #Make the aggregation pipeline
 pipeline = [
     #{"$sort":{"current_time":1}}, #to allow $last to effectively be the last
-    #{"$limit": 50}, #if we need to test it on few lines
+    {"$limit": 50}, #if we need to test it on few lines
     {"$addFields":{"tweet_len_description" : { "$strLenCP": {"$ifNull" : ["$user.description",""] }}}},
 
     #Count specific caracter inside the text of a tweet, using entities attribute
@@ -48,7 +55,6 @@ pipeline = [
                "hash_avg" : {"$avg" : "$tweet_hashtag_count"},
                "mention_avg" : {"$avg" : "$tweet_mention_count"},
                "url_avg" : {"$avg" : "$tweet_urls_count"},
-               "symbols_avg" : {"$avg" : "$tweet_symbols_count"},
                #tweet stats
                "tweet_nb" : {"$sum":1},
                "tweet_user_count":{"$max":"$user.statuses_count"} 
@@ -62,11 +68,20 @@ pipeline = [
     {"$addFields" : {"tweet_frequency" : {"$divide" : ["$tweet_user_count","$user_lifetime"] }}},
     {"$addFields" : {"friend_frequency" : {"$divide" : ["$friend_nb","$user_lifetime"] }}},
     {"$addFields" : {"aggressivity" : {"$add" : ["$tweet_frequency","$friend_frequency"] }}},
+    #Add visibility
+    {"$addFields" : {"visi_1" : {"$multiply" : ["$mention_avg",mention_cost] }}},
+    {"$addFields" : {"visi_2" : {"$multiply" : ["$hash_avg",hashtag_cost] }}},
+    {"$addFields" : {"visi_3" : {"$multiply" : ["$url_avg",url_cost] }}},
+    {"$addFields" : {"visibility" : {"$add" :["$visi_1","$visi_2","$visi_3"]}}},
     #Make the visibility attribute
-    {"$addFields" : {"visibility" : {"$add" : ["$mention_avg","$hash_avg","$url_avg","$symbols_avg"] }}},
+    #{"$addFields" : {"visibility" : {"$add" :["$mention_avg"*mention_cost,"$hash_avg"*hashtag_cost,"$url_avg"*url_cost]}}},
     #create a ff_ratio attribute
     {"$addFields" : {"ff_ratio" : {"$cond" : { "if" : { "$eq": [ "$follower_nb", 0 ] },"then" : "$friend_nb","else" : {"$divide" : ["$friend_nb","$follower_nb"] }
     }  }}},
+    {"$project":{"_id":1,"aggressivity":1,"visibility":1,"verified":1,"friend_nb":1,"listed_nb":1,"follower_nb":1,
+                 "follower_nb":1,"favorites_nb":1,"len_description":1,"tweet_nb":1,"tweet_user_count":1,
+                 "user_lifetime":1,"ff_ratio":1}},
+
     #{"$addFields" : {"ff_ratio" : {"$divide" : ["$friend_nb","$follower_nb"] }}},
     #Export it on another database
     #{"$out" : "user_db_sample"} #Sample database for small test
